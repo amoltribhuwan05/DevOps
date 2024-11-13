064821c0df9ebd3ad890fc08adbfbcb775ddbb88
Amol Tribhuwan <85682912+amoltribhuwan05@users.noreply.github.com>
2024-11-13 10:48:20 +0530

    Update calico plugin v3.29

--- Kubernates/Kubernates setup/kubemaster.sh
+++ Kubernates/Kubernates setup/kubemaster.sh
@@ -3,7 +3,7 @@ sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 sudo export KUBECONFIG=/etc/kubernetes/admin.conf
-sudo kubectl apply -f https://docs.projectcalico.org/v3.20/manifests/calico.yaml
+sudo kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.0/manifests/calico.yaml
 echo " Copy This Token And Paste it in worker Node
 ******************************************************************************************************* "
 sudo kubeadm token create --print-join-command
